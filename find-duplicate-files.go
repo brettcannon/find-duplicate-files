546a6e5ea665a07e6ac3b33d2a264e4e70365df9
Brett Cannon <brett@python.org>
2013-10-25 17:27:47 -0400

    Get hashing working
    Also ran `go fmt`

--- find-duplicate-files.go
+++ find-duplicate-files.go
@@ -1,83 +1,109 @@
 package main
 
 import "errors"
-import "path/filepath"
 import "flag"
 import "fmt"
+import "hash/fnv"
+import "io"
 import "os"
+import "path/filepath"
 
+// Hash the file at 'path'.
+func hashFile(path string) (uint64, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	buffer := make([]byte, 4096, 4096)
+	hash := fnv.New64a()
+	for {
+		n, err := file.Read(buffer)
+		if n > 0 {
+			hash.Write(buffer[:n])
+		}
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return 0, err
+		}
+	}
 
-func findFiles(directories []string) ([]string, error) {
-    files := make([]string, 0, 100)
-    // Can't use ranged 'for' as the length of directories changes during iteration.
-    for x := 0; x < len(directories); x++ {
-        directory := directories[x]
-        dirFile, err := os.Open(directory)
-        if err != nil {
-            return nil, err
-        }
-        contents, err := dirFile.Readdir(0)
-        if err != nil {
-            return nil, err
-        }
-        for _, content := range contents {
-            path := filepath.Join(directory, content.Name())
-            if content.IsDir() {
-                directories = append(directories, path)
-            } else {
-                files = append(files, path)
-            }
-        }
-    }
-
-    return files, nil
+	return hash.Sum64(), nil
 }
 
+// Find all the files contained within 'directories'.
+func findFiles(directories []string) ([]string, error) {
+	files := make([]string, 0, 100)
+	// Can't use ranged 'for' as the length of directories changes during iteration.
+	for x := 0; x < len(directories); x++ {
+		directory := directories[x]
+		dirFile, err := os.Open(directory)
+		if err != nil {
+			return nil, err
+		}
+		defer dirFile.Close()
+		contents, err := dirFile.Readdir(0)
+		if err != nil {
+			return nil, err
+		}
+		for _, content := range contents {
+			path := filepath.Join(directory, content.Name())
+			if content.IsDir() {
+				directories = append(directories, path)
+			} else {
+				files = append(files, path)
+			}
+		}
+	}
+
+	return files, nil
+}
 
 // Validate the passed-in arguments are directories.
 func validateArgs(args []string) error {
-    if len(args) < 1 {
-        return errors.New("expected 1 or more arguments")
-    }
-    for _, directory := range args {
-        dir, err := os.Open(directory)
-        if err != nil {
-            return err
-        }
-        defer dir.Close()
-        info, err := dir.Stat()
-        if err != nil {
-            return err
-        } else if !info.IsDir() {
-            return errors.New(fmt.Sprintf("%v is not a directory", directory))
-        }
-    }
+	if len(args) < 1 {
+		return errors.New("expected 1 or more arguments")
+	}
+	for _, directory := range args {
+		dir, err := os.Open(directory)
+		if err != nil {
+			return err
+		}
+		defer dir.Close()
+		info, err := dir.Stat()
+		if err != nil {
+			return err
+		} else if !info.IsDir() {
+			return errors.New(fmt.Sprintf("%v is not a directory", directory))
+		}
+	}
 
-    return nil
+	return nil
 }
 
-
 func errorExit(err error) {
-    // Would be more correct if flags.out() were publicly available instead of hard coding os.Stderr.
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
+	// Would be more correct if flags.out() were publicly available instead of hard coding os.Stderr.
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
 }
 
 // find-duplicate-files takes 1 or more directories on the command-line,
 // recurses into all of them, and prints out what files are duplicates of
 // each other.
 func main() {
-    flag.Parse()
-    directories := flag.Args()
-    err := validateArgs(directories); if err != nil {
-        errorExit(err)
-    }
-    _, err = findFiles(directories)
-    if err != nil {
-        errorExit(err)
-    }
-    // XXX hash files
-    // XXX 4096 seems to be a common buffer size for file systems and HDDs
-    // XXX collect duplicates
-    // XXX print duplicates, sorted
+	flag.Parse()
+	directories := flag.Args()
+	err := validateArgs(directories)
+	if err != nil {
+		errorExit(err)
+	}
+	_, err = findFiles(directories)
+	if err != nil {
+		errorExit(err)
+	}
+	// XXX hash files
+	// XXX 4096 seems to be a common buffer size for file systems and HDDs
+	// XXX collect duplicates
+	// XXX print duplicates, sorted
 }
--- find-duplicate-files_test.go
+++ find-duplicate-files_test.go
@@ -3,100 +3,133 @@ package main
 import "path/filepath"
 import "testing"
 
-
 var testdataTop = "testdata"
 var testdata1 = filepath.Join(testdataTop, "dir1")
 var testdata2 = filepath.Join(testdataTop, "dir2")
 
-
 func sliceToSet(stuff []string) map[string]bool {
-    set := make(map[string]bool)
-    for _, thing := range stuff {
-        set[thing] = true
-    }
-    return set
+	set := make(map[string]bool)
+	for _, thing := range stuff {
+		set[thing] = true
+	}
+	return set
 }
-
 
 // CLI
 // Error to have no arguments.
 func TestCliRequiresOneOrMoreDirectories(t *testing.T) {
-    emptySlice := make([]string, 0, 0)
-    if validateArgs(emptySlice) == nil {
-        t.Error("no arguments should be an error")
-    }
+	emptySlice := make([]string, 0, 0)
+	if validateArgs(emptySlice) == nil {
+		t.Error("no arguments should be an error")
+	}
 }
 
 // Error to specify something that doesn't exist.
 func TestCliArgsMustExist(t *testing.T) {
-    doesNotExist := []string{"Totally bogus file name"}
-    if validateArgs(doesNotExist) == nil {
-        t.Error("non-existent arguments should be an error")
-    }
+	doesNotExist := []string{"Totally bogus file name"}
+	if validateArgs(doesNotExist) == nil {
+		t.Error("non-existent arguments should be an error")
+	}
 }
 
 // Error to specify a file.
 func TestCliArgsMustBeDirectories(t *testing.T) {
-    file := []string{filepath.Join(testdata1, "intra-same")}
-    if validateArgs(file) == nil {
-        t.Error("files are not valid arguments")
-    }
+	file := []string{filepath.Join(testdata1, "intra-same")}
+	if validateArgs(file) == nil {
+		t.Error("files are not valid arguments")
+	}
 }
 
 // Succeed if all arguments are directories.
 func TestCliArgsDirectoriesOK(t *testing.T) {
-    dir := []string{testdataTop, testdata1}
-    if validateArgs(dir) != nil {
-        t.Error("directories are acceptable")
-    }
+	dir := []string{testdataTop, testdata1}
+	if validateArgs(dir) != nil {
+		t.Error("directories are acceptable")
+	}
 }
 
 // Directory traversal
 // Directory with only files.
 func TestFindFiles(t *testing.T) {
-    dir := []string{testdata2}
-    files, err := findFiles(dir)
-    if err != nil {
-        t.Fatal(err)
-    }
-    fileSet := sliceToSet(files)
-    if len(fileSet) != 2 {
-        t.Fatalf("found %v files instead of 2", len(fileSet))
-    }
-    file1 := filepath.Join(testdata2, "inter-diff")
-    file2 := filepath.Join(testdata2, "inter-same")
-    for _, file := range []string{file1, file2} {
-        if !fileSet[file] {
-            t.Errorf("%v not found in %v", file1, fileSet)
-        }
-    }
+	dir := []string{testdata2}
+	files, err := findFiles(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fileSet := sliceToSet(files)
+	if len(fileSet) != 2 {
+		t.Fatalf("found %v files instead of 2", len(fileSet))
+	}
+	file1 := filepath.Join(testdata2, "inter-diff")
+	file2 := filepath.Join(testdata2, "inter-same")
+	for _, file := range []string{file1, file2} {
+		if !fileSet[file] {
+			t.Errorf("%v not found in %v", file1, fileSet)
+		}
+	}
 }
 
 // Directory containing subdirectories.
 func TestFindRecursively(t *testing.T) {
-    dir := []string{testdataTop}
-    files, err := findFiles(dir)
-    if err != nil {
-        t.Fatal(err)
-    }
-    fileSet := sliceToSet(files)
-    if len(fileSet) != 8 {
-        t.Fatalf("found %v files instead of 8", len(fileSet))
-    }
-    if !fileSet[filepath.Join(testdata1, "intra-diff1")] {
-        t.Error("didn't find the intra-diff1 file")
-    }
+	dir := []string{testdataTop}
+	files, err := findFiles(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fileSet := sliceToSet(files)
+	if len(fileSet) != 8 {
+		t.Fatalf("found %v files instead of 8", len(fileSet))
+	}
+	if !fileSet[filepath.Join(testdata1, "intra-diff1")] {
+		t.Error("didn't find the intra-diff1 file")
+	}
 }
 
 // Multiple directories.
 func TestFindInMultipleDirectories(t *testing.T) {
-    dirs := []string{testdata1, testdata2}
-    files, err := findFiles(dirs)
-    if err != nil {
-        t.Fatal(err)
-    }
-    fileSet := sliceToSet(files)
-    if len(fileSet) != 8 {
-        t.Fatalf("found %v files instead of 8", len(fileSet))
-    }
+	dirs := []string{testdata1, testdata2}
+	files, err := findFiles(dirs)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fileSet := sliceToSet(files)
+	if len(fileSet) != 8 {
+		t.Fatalf("found %v files instead of 8", len(fileSet))
+	}
+}
+
+// Hashing
+// Error out if the path is bogus.
+func TestHashingUnknown(t *testing.T) {
+	_, err := hashFile("bogus file name")
+	if err == nil {
+		t.Error("non-existent file didn't trigger an error")
+	}
+}
+
+// Verify hashing works.
+func TestHasing(t *testing.T) {
+	intraSame1, err := hashFile(filepath.Join(testdata1, "intra-same1"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	intraSame2, err := hashFile(filepath.Join(testdata1, "intra-same2"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if intraSame1 != intraSame2 {
+		t.Errorf("%v != %v", intraSame1, intraSame2)
+	}
+
+	intraDiff1, err := hashFile(filepath.Join(testdata1, "intra-diff1"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	intraDiff2, err := hashFile(filepath.Join(testdata1, "intra-diff2"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if intraDiff1 == intraDiff2 {
+		t.Errorf("%v == %v", intraDiff1, intraDiff2)
+	}
 }
